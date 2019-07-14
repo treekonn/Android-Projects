96267ff1c39cd54a16dc377792eee28acf91f70e
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-14 10:34:11 +0300

    feat: @VoiceAssistant add Main layout and basic logic

--- VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/MainActivity.kt
+++ VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/MainActivity.kt
@@ -1,12 +1,17 @@
 package by.enolizard.voiceassistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_send.setOnClickListener {
+            txt_area.text = txt_area.text.toString() + "\n<< " + txt_question.text.toString()
+        }
     }
 }
