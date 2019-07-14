eafcdfc315de89da3d6a862173200d484f5810c9
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-14 09:31:22 +0300

    feat: @VKFriends add getting VK fingerprint

--- VKFriends/app/src/main/java/by/yakivan/vkfriends/SampleApplication.kt
+++ VKFriends/app/src/main/java/by/yakivan/vkfriends/SampleApplication.kt
@@ -1,12 +1,21 @@
 package by.yakivan.vkfriends
 
 import android.app.Application
+import android.util.Log
 import com.vk.api.sdk.VK
+import com.vk.api.sdk.utils.VKUtils
+import org.jetbrains.annotations.TestOnly
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         VK.initialize(applicationContext)
+//        getVKfingeprint()
+    }
+
+    fun getVKFingerprint() {
+        var fingerprint = VKUtils.getCertificateFingerprint(this, packageName)
+        ;
     }
 }
