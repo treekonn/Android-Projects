bce750f67ea20034972a6eea7747ab48d21d030c
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-15 11:13:51 +0300

    feat: &VoiceAssistant setup textToSpeach

--- VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/AI.kt
+++ VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/AI.kt
@@ -2,26 +2,26 @@ package by.enolizard.voiceassistant
 
 class AI {
     companion object {
-        fun getAnswer(question: String): String {
+        fun getAnswer(reqQuestion: String): String {
             val database: MutableMap<String, String> = HashMap()
-            database.put("привет", "И вам здрасте")
-            database.put("как дела", "Да вроде ничего")
-            database.put("как тебя зовут", "Я голосовой помощник")
-            database.put("кто тебя создал", "Бог не прилогал к этому руку")
+            database.put("hi", "Hi, brother. ")
+            database.put("hello", "Hello, human. ")
+            database.put("how are you", "Fine, Are you? ")
+            database.put("what is you name", "I am a droid! ")
 
-            val es = question.toLowerCase()
+            var reqQuestionLower = reqQuestion.toLowerCase()
             val answers = StringBuffer()
 
-            for (ds in database.keys) {
-                if (es.contains(ds))
-                    database.get(ds)?.let { answers.append(it) }
+            for (question in database.keys) {
+                if (reqQuestionLower.contains(question))
+                    database[question]?.let { answers.append(it) }
             }
 
             if (answers.isNotEmpty()) {
                 return answers.toString()
             }
 
-            return "Ok, understand!"
+            return "Ok, understand! "
         }
     }
 }
--- VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/MainActivity.kt
+++ VoiceAssistant/app/src/main/java/by/enolizard/voiceassistant/MainActivity.kt
@@ -1,16 +1,19 @@
 package by.enolizard.voiceassistant
 
+import android.os.Build
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val rvDialog by lazy { rv_dialog }
     private val txtQuestion by lazy { txt_question }
     private val btnSend by lazy { btn_send }
     private val msgContoller = MessageController()
-
+    private lateinit var tts: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +27,15 @@ class MainActivity : AppCompatActivity() {
             val userMsg = txtQuestion.text.toString()
             txtQuestion.setText("")
             msgContoller.addMsg(userMsg, Message.USER)
-
             val answer = AI.getAnswer(userMsg)
             msgContoller.addMsg(answer, Message.ASSISTANT)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                tts.speak(answer, TextToSpeech.QUEUE_FLUSH, null, null)
+            }
+        }
+
+        tts = TextToSpeech(this) {
+            tts.language = Locale.ENGLISH
         }
     }
 }
