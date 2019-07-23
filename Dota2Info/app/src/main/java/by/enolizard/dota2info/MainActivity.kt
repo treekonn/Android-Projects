4d634eabe6a41735863bdbe756052f79a75159dc
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-23 09:43:21 +0300

    feat: Dota2Info init

---
+++ Dota2Info/app/src/main/java/by/enolizard/dota2info/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.enolizard.dota2info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
