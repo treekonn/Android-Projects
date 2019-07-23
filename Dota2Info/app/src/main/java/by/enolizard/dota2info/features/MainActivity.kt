d646f7552075d07ee980044362f83246ad807f49
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-07-23 10:27:51 +0300

    feat: Dota2Info add icons, base setup BottomNavigationView

--- Dota2Info/app/src/main/java/by/enolizard/dota2info/features/MainActivity.kt
+++ Dota2Info/app/src/main/java/by/enolizard/dota2info/features/MainActivity.kt
@@ -1,12 +1,16 @@
-package by.enolizard.dota2info
+package by.enolizard.dota2info.features
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.enolizard.dota2info.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bnv_hero_attr_types.itemIconTintList = null
     }
 }
